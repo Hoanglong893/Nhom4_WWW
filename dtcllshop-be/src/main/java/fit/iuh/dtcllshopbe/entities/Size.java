bef26b6766d15b21fec0e8c3b1e3852e07030bdd
tranphuongtri <gunnybboss@gmail.com>
2026-05-16 17:09:39 +0700

    Size update

--- dtcllshop-be/src/main/java/fit/iuh/dtcllshopbe/entities/Size.java
+++ dtcllshop-be/src/main/java/fit/iuh/dtcllshopbe/entities/Size.java
@@ -1,6 +1,7 @@
 package fit.iuh.dtcllshopbe.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import fit.iuh.dtcllshopbe.enums.SizeName;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
